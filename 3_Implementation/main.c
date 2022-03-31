a1c2e6d08e9ba9ae79b660794776a175bcd88631
lethinjoo <102732132+lethinjoo@users.noreply.github.com>
2022-03-31 23:55:26 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -170,9 +170,7 @@ while(another=='y'||another=='Y')
 {
     printf("\n\n\t\tEnter ID number: ");
     scanf("%d",&e.id);
-
     printf("\n\n\t\tEnter Full Name of Employee: ");
-    fflush(stdin);
     fgets(e.name,100,stdin); //fgets takes an extra \n character as input
     e.name[strlen(e.name)-1]='\0';
 
