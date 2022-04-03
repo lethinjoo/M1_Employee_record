da3b1bce0301b3d57a08fa5da3fbe66cc0e6b58b
lethinjoo <102732132+lethinjoo@users.noreply.github.com>
2022-04-03 17:18:07 +0530

    Create employeerecord.c

---
+++ 3_Implementation/test/employeerecord.c
@@ -0,0 +1,30 @@
+#include "unity.h"
+extern void add(FILE * fp); 
+extern FILE * del(FILE * fp);
+extern void modify(FILE * fp);
+extern void displayList(FILE * fp);
+extern void searchRecord(FILE *fp);
+extern void printChar(char ch,int n);
+extern void printHead();
+
+void setUp(void)
+{
+
+}
+void tearDown(void)
+{
+
+}
+int main(void)
+{
+    UnityBegin(NULL);
+    RUN_TEST(add);
+    RUN_TEST(del);
+    RUN_TEST(modify);
+    RUN_TEST(displayList);
+    RUN_TEST(searchRecord);
+    RUN_TEST(printChar);
+    RUN_TEST(printHead);
+    return(UnityEnd());
+
+}
